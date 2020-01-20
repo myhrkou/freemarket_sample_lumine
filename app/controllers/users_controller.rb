b240a939062260d8647196a0b19bbbf121ed08d1
myhrkou <56810406+myhrkou@users.noreply.github.com>
2020-01-20 16:52:36 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def index
+    require 'aws-sdk'
+
+    s3=Aws::Resource.new(region:'ap-northeast-1')
+
+    obj=s3.bucket('freemarket2020').object('fmarket_logo_red.svg')
+
+    obj.get(response_target:'./uploads/images/fmarket_logo_red.svg')
   end
 end
