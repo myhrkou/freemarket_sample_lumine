96a1e013c4831c833fb617a22212fb4e1813bab8
myhrkou <56810406+myhrkou@users.noreply.github.com>
2020-01-23 17:27:04 +0900

    全部

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def index
   end
+  def edit
+  end
+  def update
+    if current_user.update!(user_params)
+      render :edit
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(address_attributes: [:id, :zip, :prefecture, :city, :number, :building, :user_id],
+    )
+  end
 end
