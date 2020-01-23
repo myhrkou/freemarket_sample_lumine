ab8d3f29bdc414536161b956dab5e63aec0e090b
myhrkou <56810406+myhrkou@users.noreply.github.com>
2020-01-23 15:47:58 +0900

    マイページ調整

--- app/controllers/mypages_controller.rb
+++ app/controllers/mypages_controller.rb
@@ -1,9 +1,15 @@
 class MypagesController < ApplicationController
+  before_action :set_card
   def mypage
-    @card = Card.where(user_id: current_user.id).first
   end
   def mypagelogout
   end
   def card
+  end
+
+  private
+
+  def set_card
+    @card = Card.where(user_id: current_user.id).first
   end
 end
