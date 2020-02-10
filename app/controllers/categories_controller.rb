351c022f45d2eb8ffcad0ebbd83149ac20014919
myhrkou <56810406+myhrkou@users.noreply.github.com>
2020-02-10 14:13:31 +0900

    Update categories_controller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,19 +6,19 @@ class CategoriesController < ApplicationController
 
   def category_root
     @category_ids = Category.find(params[:format]).indirect_ids
-    @items=@category_ids.map{|category_id| Item.find_by(category_id: category_id)}
+    @items=@category_ids.map{|category_id| Item.find_by(category_id: category_id)}.compact
     @category=@categories[params[:format].to_i-1]
   end
 
   def category_child
     @category_ids = Category.find(params[:format]).child_ids
-    @items=@category_ids.map{|category_id| Item.find_by(category_id: category_id)}
+    @items=@category_ids.map{|category_id| Item.find_by(category_id: category_id)}.compact
     @category=@categories[params[:format].to_i-1]
   end
 
   def category_grandchild
     @category_id = Category.where(id: params[:format]).ids.first
-    @items = Item.where(category_id: @category_id)
+    @items = Item.where(category_id: @category_id).compact
     @category=@categories[params[:format].to_i-1]
   end
 
