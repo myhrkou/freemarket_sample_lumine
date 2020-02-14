9f1aba64e2de5a5bf34b4612359b3902bd91dc74
myhrkou <56810406+myhrkou@users.noreply.github.com>
2020-02-14 13:22:03 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,4 @@
 FactoryBot.define do
-  factory :user2 do
-    nickname { "abebuu" }
-    email { Faker::Internet.email }
-    password { "00000000" }
-    password_confirmation { "00000000" }
-    first_name { "ひろし" }
-    last_name { "佐藤" }
-    yomi_first_name { "ヒロシ" }
-    yomi_last_name { "サトウ" }
-    birth_day { "1996,05,30" }
-    phone_number { "00011112222" }
-  end
-
   factory :user do
     nickname { "abebuu" }
     email { Faker::Internet.email }
